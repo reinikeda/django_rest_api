46900ee5bd67028bad9be28158355db3b677dab1
reinikeda <reinikeda@gmail.com>
2023-03-06 11:46:01 +0200

    BandSerializer fixed

--- django_rest_api/playlist/serializers.py
+++ django_rest_api/playlist/serializers.py
@@ -3,9 +3,7 @@ from . import models
 
 
 class BandSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
-    user_id = serializers.ReadOnlyField(source='user.id')
 
     class Meta:
         model = models.Band
-        fields = ('id', 'band', 'user', 'user_id')
+        fields = ('id', 'band')
